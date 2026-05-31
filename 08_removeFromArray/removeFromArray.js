6a257558a1b559c6c34261898970d21f936e0711
Pheonix Desktop <168647197+rohanp29@users.noreply.github.com>
2026-05-31 15:33:32 -0400

    Tested excercise 6 and implemented and tested excercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        arr = arr.filter(el => el !== arguments[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
