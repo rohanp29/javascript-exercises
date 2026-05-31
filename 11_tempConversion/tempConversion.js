0066ee06c065a8b8a6e821c7487cba1c84b5615e
Pheonix Desktop <168647197+rohanp29@users.noreply.github.com>
2026-05-31 16:00:20 -0400

    Finished exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = (farenheit) => {
+    return Math.round((farenheit - 32) * 5/9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (celsius) => {
+    return Math.round(((celsius * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
