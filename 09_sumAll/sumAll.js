d9075b03dbc18a9bfee37c168d04a24f48b7c554
Pheonix Desktop <168647197+rohanp29@users.noreply.github.com>
2026-05-31 15:46:47 -0400

    finished exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
 
+    let order = [a, b].sort((a, b) => a - b);
+    let sum = 0;
+    for (let i = order[0]; i <= order[1]; i++) {
+        if (i < 0 || !Number.isInteger(i)) {
+            return 'ERROR';
+        }
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
